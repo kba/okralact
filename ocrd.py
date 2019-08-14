be7707936f543e3aa99472ac222d4885211906eb
Konstantin Baierer <unixprog@gmail.com>
2019-08-14 11:56:28 +0200

    add simple click cli to start server

--- ocrd.py
+++ ocrd.py
@@ -1,3 +1,18 @@
 from app import app
+import click
 
-app.run(debug=True, host='0.0.0.0')
+@click.command('okralact')
+@click.version_option()
+@click.option('-h', '--host',
+              default='0.0.0.0',
+              help='Host to run on',
+              show_default=True)
+@click.option('-p', '--port',
+              default=5000,
+              help='Port to run on',
+              show_default=True)
+def okralact_cli(host, port):
+    app.run(debug=True, host=host, port=port)
+
+if __name__ == '__main__':
+    okralact_cli()
